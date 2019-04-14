dd79a93752ff538ad4270709dc746e1344185de7
Sang2306 <sangvnorg@gmail.com>
2019-04-14 09:32:54 +0700

    them ham checkNewPassword

--- login/admin.py
+++ login/admin.py
@@ -6,17 +6,18 @@ class UserAdmin(admin.ModelAdmin):
     """
         Hien thi thong tin co ban cua user, va tim kiem thong tin user
     """
-    search_fields = ["user_ID",]
-    ordering = ["user_ID",]
+    search_fields = ["user_ID", ]
+    ordering = ["user_ID", ]
     list_display = ['user_ID', "name", "password"]
-
-#Tao model quan tri mail
+    
+# Tao model quan tri mail
 class MailAdmin(admin.ModelAdmin):
     """
         Hien thi thong tin co ban cua mail VD user_ID, mail address
     """
-    search_fields = ["user",]
-    list_display = ["user_id","mail_address"]
+    search_fields = ["user", ]
+    list_display = ["user_id", "mail_address"]
+
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
