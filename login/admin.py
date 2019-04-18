8bbf0f4708d99ffeb60b1680a128814fe666e9f0
Sang2306 <sangvnorg@gmail.com>
2019-04-18 15:38:59 +0700

    add 'get_number_of_joins' in admin.py

--- login/admin.py
+++ login/admin.py
@@ -19,8 +19,28 @@ class MailAdmin(admin.ModelAdmin):
     list_display = ["user_id", "mail_address"]
 
 
+#Tao model quan tri cho Activity
+class ActivityAdmin(admin.ModelAdmin):
+    """
+        Hien thi thong tin cua hoat dong va tim kiem hoat dong dua vao thoi gian
+    """
+    list_display = ["activity_ID", "name", "start_date", "get_number_of_joins"]
+
+    def get_number_of_joins(self, obj):
+        """
+            Trả về số  lượng người tham gia
+        """
+        return len(obj.user_set.all())
+    get_number_of_joins.short_description = "Số lượng tham dự"
+
+    def get_number_of_register(self, obj):
+        """
+            Trả về  số  lượng đăng ký
+        """
+        return 0
+
 # Register your models here.
 admin.site.register(User, UserAdmin)
 admin.site.register(Mail, MailAdmin)
 admin.site.register(Role)
-admin.site.register(Activity)
+admin.site.register(Activity, ActivityAdmin)
