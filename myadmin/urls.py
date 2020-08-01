47612c5eff11c3ab60e4922d6fb3fe8bb1575c86
Duong <vanduongk1309@gmail.com>
2020-08-01 21:19:44 +0700

    Hoàn thành chức năng login cho superuser

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -5,8 +5,9 @@ app_name = "myadmin"
 
 urlpatterns = [
     path('', views.dashboards, name='dashboard'),
-    path('list-user', views.user_list, name='list_user'),
-    path('create-user', views.create_user, name='create_user'),
+    path('login', views.login_myadmin, name='login'),
+    path('list-user/', views.user_list, name='list_user'),
+    path('create-user/', views.create_user, name='create_user'),
     path('update-user/<int:pk>', views.update_user, name='update_user'),
     path('delete-user/<int:pk>', views.delete_user, name='delete_user')
 ]
--- myadmin/views.py
+++ myadmin/views.py
@@ -1,18 +1,88 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from django.template.loader import render_to_string
-
 from .forms import UserForm
 from login.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.datastructures import MultiValueDictKeyError
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth import models
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 
-from django.http import HttpResponse
+
 # Create your views here.
+
+# def login_myadmin(request):
+#     context = {}
+#     try:
+#         context = {
+#             'name': request.POST['name'],
+#             'password': request.POST['password'],
+#         }
+#         user = models.User.objects.get(username__iexact=context['name'])
+#         if user.password == context['password'] and user.is_active == True and user.is_superuser == True and user.is_staff == True:
+#             save_session_data(request)
+#             return HttpResponseRedirect(reverse('myadmin:dashboard'))
+#         else:
+#             context['login-status'] = 'Sai mật khẩu'
+#     except MultiValueDictKeyError:
+#         pass
+#     except ObjectDoesNotExist:
+#         context['user_not_found'] = 'Người dùng khoonng tồn tại'
+#     return render(request, 'myadmin/login_myadmin.html', context)
+
+def login_myadmin(request):
+    context = {}
+    try:
+        username = request.POST['name']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None and user.is_superuser:
+            login(request, user)
+            save_session_data(request)
+            return HttpResponseRedirect(reverse('myadmin:dashboard'))
+        else:
+            context['login-status'] = 'Sai mật khẩu'
+    except MultiValueDictKeyError:
+        pass
+    except ObjectDoesNotExist:
+        context['user_not_found'] = 'Người dùng khoonng tồn tại'
+    return render(request, 'myadmin/login_myadmin.html', context)
+
+
+@login_required(login_url='myadmin:login')
+def save_session_data(request):
+    request.session['name'] = request.POST['name']
+    request.session['password'] = request.POST['password']
+    request.session.set_expiry(60)
+
+
+
+
+@login_required(login_url='myadmin:login')
 def dashboards(request):
     return render(request, 'myadmin/index.html')
+    
+        
+        
 
+@login_required(login_url='myadmin:login')
 def user_list(request):
-    userlist = User.objects.all()
-    return render(request, 'myadmin/user_list.html', {'users': userlist})
+    user_list = User.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(user_list, 2)
+    try:
+        users = paginator.page(page)
+    except PageNotAnInteger:
+        users = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
+
+    return render(request, 'myadmin/user_list.html', { 'users': users })
 
 
 def create_user(request):
@@ -23,7 +93,7 @@ def create_user(request):
 
     return save_user_form(request, form, 'myadmin/includes/partial_create_user.html')
     
-
+@login_required(login_url='myadmin:login')
 def save_user_form(request, form, template_name):
     data = dict()
     if request.method == 'POST':
@@ -40,7 +110,7 @@ def save_user_form(request, form, template_name):
     data['html_form'] = render_to_string(template_name, context, request=request)
     return JsonResponse(data)
 
-
+@login_required(login_url='myadmin:login')
 def delete_user(request, pk):
     user = get_object_or_404(User, pk=pk)
     data = dict()
@@ -56,27 +126,7 @@ def delete_user(request, pk):
         data['html_form'] = render_to_string('myadmin/includes/partial_delete_user.html', context, request=request)
     return JsonResponse(data)
 
-
-
-
-def book_delete(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    data = dict()
-    if request.method == 'POST':
-        book.delete()
-        data['form_is_valid'] = True
-        books = Book.objects.all()
-        data['html_book_list'] = render_to_string('books/includes/partial_book_list.html', {
-            'books': books
-        })
-    else:
-        context = {'book': book}
-        data['html_form'] = render_to_string('books/includes/partial_book_delete.html', context, request=request)
-    return JsonResponse(data)
-
-
-
-
+@login_required(login_url='myadmin:login')
 def update_user(request, pk):
     user = get_object_or_404(User, pk=pk)
     if request.method == 'POST':
@@ -84,3 +134,5 @@ def update_user(request, pk):
     else:
         form = UserForm(instance=user)
     return save_user_form(request, form, 'myadmin/includes/partial_update_user.html')
+
+
